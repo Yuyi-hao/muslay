f9c300607eaf389ac052e1187ee3d309648cd8a7
Yuyi-Hao <roshanisingh2354@gmail.com>
2025-06-30 20:47:00 +0530

    [CHG] api endpoint prefixed with /api

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('notifications/', include('notifications.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/notifications/', include('notifications.urls')),
 ]
